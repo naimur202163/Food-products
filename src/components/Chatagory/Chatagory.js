31156763943be70fd09d7eadb23c26eadb4088c2
unknown <you@example.com>
2021-12-18 11:19:58 +0600

    productts

--- src/components/Chatagory/Chatagory.js
+++ src/components/Chatagory/Chatagory.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Chatagory = () => {
     return (
         <div>
-            
+
         </div>
     );
 };
--- src/components/Home/Home/Home.js
+++ src/components/Home/Home/Home.js
@@ -2,10 +2,7 @@ import React, { useEffect } from 'react';
 import AppBanner from '../AppBanner/AppBanner';
 import Header from '../Header/Header';
 import MainProducts from '../MainProducts/MainProducts';
-import MiddleBanner from '../PizzaBanner/MiddleBanner/MiddleBanner';
-import PizzaBanner from '../PizzaBanner/PizzaBanner';
 import Products from './../Products/Products';
-import PizzaBox from './../PizzaBox/PizzaBox';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 const Home = () => {
@@ -14,13 +11,9 @@ const Home = () => {
     }, [])
     return (
         <div>
-            <PizzaBanner />
             <Products />
             <MainProducts />
-            <PizzaBox />
-            <MiddleBanner />
 
-            <AppBanner />
 
         </div>
     );
